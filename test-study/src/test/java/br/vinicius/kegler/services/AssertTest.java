39834a50e8184c45a84835ca41536c984fca1667
Vinícius Kegler <viniciuskegler.flexmobile@gmail.com>
2023-10-02 13:05:22 -0300

    Before and After and Rules
    Messed around with before, after and the class variations (BeforeClass,
    AfterClass) and further use of error Rules.

--- test-study/src/test/java/br/vinicius/kegler/services/AssertTest.java
+++ test-study/src/test/java/br/vinicius/kegler/services/AssertTest.java
@@ -1,24 +1,42 @@
 package br.vinicius.kegler.services;
 
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class AssertTest {
 
 	
-//	@Test
+	@Test
 	public void test() {
-		Assert.assertTrue(true);
-		Assert.assertFalse(false);
-		Assert.assertEquals(1, 1);
-		Assert.assertEquals(0.51, 0.51, 0.01);
-		
-		int i = 5;
-		Integer i2 = 5;
-		Assert.assertEquals(Integer.valueOf(i), i2);
-		Assert.assertEquals(i, i2.intValue());
-		
-		Assert.assertEquals("bola", "bola");
+		Float a = null;
+		Float b = null;
+		Float c = null;
+		String pcSt = formatPercentage(a / (b + c));
+		Assert.assertTrue(1 == 1);
+	}
+
+	
+	public static String formatPercentage(float f) {
+		return formatFloat(f * 100f, 2) + "%";
 	}
 	
+	public static String formatFloat(double f, int maxCasas) {
+		return format(f, maxCasas);
+	}
+	
+	public static String format(Object f, int maxCasas) {
+		if(Double.isNaN((Double) f)){
+			f = 0;
+		}
+		DecimalFormat df = new DecimalFormat();
+		df.setMaximumFractionDigits(maxCasas);
+		df.setGroupingUsed(false);
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols();
+		dfs.setDecimalSeparator('.');
+		df.setDecimalFormatSymbols(dfs);
+		return df.format(f);
+	}
 }
--- test-study/src/test/java/br/vinicius/kegler/services/RentalServiceTest.java
+++ test-study/src/test/java/br/vinicius/kegler/services/RentalServiceTest.java
@@ -1,57 +1,54 @@
 package br.vinicius.kegler.services;
 
-import org.junit.Assert;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-import static org.hamcrest.CoreMatchers.equalTo;
 
 import br.vinicius.kegler.entities.Movie;
-import br.vinicius.kegler.entities.Rental;
 import br.vinicius.kegler.entities.User;
 import br.vinicius.kegler.exceptions.RentalException;
-import br.vinicius.kegler.utils.DateUtils;
 
 public class RentalServiceTest {
+	
+	private RentalService rentalService;
 	
 	@Rule
 	public ErrorCollector errorCollector = new ErrorCollector();
 
-
-	/*	
-	@Test
-	public void rentalTest() throws Exception {
-		RentalService ls = new RentalService();
-		
-		Integer stockQuantity = (int) Math.round((Math.random() * 15));
-		Double moviePrice = Math.random() * 26;
-		
-		
-		User user = new User("John");
-		Movie movie = new Movie("Test of tests 1", stockQuantity, moviePrice);
-		Rental rent = ls.rentMovie(user, movie);
-		
-		errorCollector.checkThat(movie.getRentalPrice(), is(equalTo(moviePrice)));
-		errorCollector.checkThat(DateUtils.isSameDate(rent.getRentDate(), new Date()), is(true));
-		errorCollector.checkThat(DateUtils.isSameDate(rent.getReturnDate(), DateUtils.getDateWithDaysDifference(1)), is(true));
+	@Before
+	public void setup() {
+		rentalService = new RentalService();
+		System.out.println("Before");
 	}
- 	*/
+
+	@After
+	public void tearDown() {
+		System.out.println("After");
+	}
+
+	@BeforeClass
+	public static void setupClass() {
+		System.out.println("Before Class");
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println("After Class");
+	}
 	
 	@Test
 	public void rentalTestOutofStock() {
-		RentalService ls = new RentalService();
 		User user = new User("Johnson");
 		Movie movie = new Movie("Test of tests 2", 0, Math.random() * 26);
 		try {
-			ls.rentMovie(user, movie);
+			rentalService.rentMovie(user, movie);
 			errorCollector.addError(new AssertionError("Should've failed due to no stock."));
 		}catch (RentalException e) {
 			errorCollector.checkThat(e.getMessage(), is(movie.getName() + " is out of stock."));
@@ -60,11 +57,10 @@ public class RentalServiceTest {
 	
 	@Test
 	public void rentalTestEmptyName() {
-		RentalService ls = new RentalService();
 		User user = new User("Test fail");
 		Movie movie = new Movie("Test of tests 2", 10, Math.random() * 26);
 		try {
-			ls.rentMovie(user, movie);
+			rentalService.rentMovie(user, movie);
 			errorCollector.addError(new AssertionError("Should've failed due to empty name."));
 		}catch (RentalException e) {
 			errorCollector.checkThat(e.getMessage(), is("User name is empty"));
@@ -73,11 +69,10 @@ public class RentalServiceTest {
 	
 	@Test
 	public void rentalTestEmptyUser() {
-		RentalService ls = new RentalService();
 		User user = null;
 		Movie movie = new Movie("Test of tests 2", 10, Math.random() * 26);
 		try {
-			ls.rentMovie(user, movie);
+			rentalService.rentMovie(user, movie);
 			errorCollector.addError(new AssertionError("Should've failed due to empty user."));
 		}catch (RentalException e) {
 			errorCollector.checkThat(e.getMessage(), is("No user selected!"));
@@ -86,11 +81,10 @@ public class RentalServiceTest {
 	
 	@Test
 	public void rentalTestEmptyMovie() {
-		RentalService ls = new RentalService();
 		User user = new User("Johnson");
 		Movie movie = null;
 		try {
-			ls.rentMovie(user, movie);
+			rentalService.rentMovie(user, movie);
 			errorCollector.addError(new AssertionError("Should've failed due to empty movie."));
 		}catch (RentalException e) {
 			errorCollector.checkThat(e.getMessage(), is("No movie selected!"));
